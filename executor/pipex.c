6551cd82d3fcabef7210f5a5e699d6db842aab7f
Mirjam Goedkoop <mgoedkoo@f0r1s11.codam.nl>
2023-08-07 18:26:10 +0200

    integrated nr_pipes

--- executor/pipex.c
+++ executor/pipex.c
@@ -6,20 +6,20 @@
 /*   By: mgoedkoo <mgoedkoo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/31 21:13:00 by mgoedkoo      #+#    #+#                 */
-/*   Updated: 2023/08/02 15:58:52 by mgoedkoo      ########   odam.nl         */
+/*   Updated: 2023/08/07 16:37:58 by mgoedkoo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 // waits for all child processes and gets exit status of last one
-static int	wait_for_children(int size, pid_t *pid)
+static int	wait_for_children(t_data *data, pid_t *pid)
 {
 	int	stat;
 	int	i;
 
 	i = 0;
-	while (i < size)
+	while (i < data->nr_pipes + 1)
 	{
 		if (pid[i] > 0)
 			waitpid(pid[i], &stat, 0);
@@ -29,55 +29,14 @@ static int	wait_for_children(int size, pid_t *pid)
 	return (WEXITSTATUS(stat));
 }
 
-// expands cmd, creates 1st child process
-static void	first_cmd(t_data *data, int orig_pipe[], pid_t *pid)
-{
-	t_cmd	*cmd;
-
-	cmd = data->cmds;
-	expand_cmd(cmd->args);
-	cmd->fd_io = redirects(cmd);
-	if (cmd->args)
-	{
-		pid[0] = fork();
-		if (pid[0] == -1)
-			exit_error(NULL, NULL, 1);
-		if (pid[0] == 0)
-			first_child(data, cmd, orig_pipe);
-	}
-	close(orig_pipe[1]);
-}
-
-// expands cmd, creates last child process
-static void	last_cmd(t_data *data, int orig_pipe[], pid_t last_pid)
-{
-	t_cmd	*cmd;
-
-	cmd = find_last_cmd(data->cmds);
-	expand_cmd(cmd->args);
-	cmd->fd_io = redirects(cmd);
-	if (cmd->args)
-	{
-		last_pid = fork();
-		if (last_pid == -1)
-			exit_error(NULL, NULL, 1);
-		if (last_pid == 0)
-			last_child(data, cmd, orig_pipe);
-	}
-	close(orig_pipe[0]);
-}
-
 // loops through cmds, expands them, creates 2nd pipe and middle child processes
-// QUESTION: can vars in cmds struct help wit norm errors?
-static int	multi_pipes(t_data *data, int size, int pipe_in[], pid_t *pid)
+static int	multi_pipes(t_data *data, int pipe_in[], pid_t *pid, int i)
 {
 	t_cmd	*cmd;
 	int		pipe_out[2];
-	int		i;
 
 	cmd = (data->cmds)->next;
-	i = 1;
-	while (i < size - 1)
+	while (i < data->nr_pipes)
 	{
 		expand_cmd(cmd->args);
 		cmd->fd_io = redirects(cmd);
@@ -101,26 +60,29 @@ static int	multi_pipes(t_data *data, int size, int pipe_in[], pid_t *pid)
 }
 
 // creates process ids, initializes them and creates 1st pipe
-int	pipex(t_data *data, int size)
+int	pipex(t_data *data)
 {
 	pid_t	*pid;
 	int		orig_pipe[2];
 	int		i;
 
-	pid = ft_calloc(size, sizeof(pid_t));
+	pid = ft_calloc(data->nr_pipes + 1, sizeof(pid_t));
 	if (!pid)
 		exit_error(NULL, NULL, 1);
 	if (pipe(orig_pipe) == -1)
 		exit_error(NULL, NULL, 1);
 	i = 0;
-	while (i < size)
+	while (i < data->nr_pipes + 1)
 	{
 		pid[i] = -1;
 		i++;
 	}
-	first_cmd(data, orig_pipe, pid);
-	if (size > 2)
-		orig_pipe[0] = multi_pipes(data, size, orig_pipe, pid);
-	last_cmd(data, orig_pipe, pid[size - 1]);
-	return (wait_for_children(size, pid));
+	first_cmd(data, orig_pipe, pid[0]);
+	if (data->nr_pipes > 1)
+	{
+		i = 1;
+		orig_pipe[0] = multi_pipes(data, orig_pipe, pid, i);
+	}
+	last_cmd(data, orig_pipe, pid[data->nr_pipes]);
+	return (wait_for_children(data, pid));
 }
--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/10 13:52:37 by arommers      #+#    #+#                 */
-/*   Updated: 2023/08/07 15:45:54 by arommers      ########   odam.nl         */
+/*   Updated: 2023/08/07 16:46:14 by mgoedkoo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,18 +124,18 @@ char		*expand_str(char *str, int isheredoc);
 
 // Executor functions
 
-int			cmds_size(t_cmd *cmds);
 void		executor(t_data *data);
 int			*redirects(t_cmd *cmd);
 int			single_cmd(t_data *data);
-t_cmd		*find_last_cmd(t_cmd *cmds);
-int			pipex(t_data *data, int size);
+int			pipex(t_data *data);
 void		only_child(t_data *data, t_cmd *cmd);
 void		run_cmd(t_data *data, char **cmd_argv);
 void		heredoc(t_cmd *cmds, t_lexer *heredoc);
 void		exit_error(char *cmd, char *err_msg, int exit_code);
 void		last_child(t_data *data, t_cmd *cmd, int pipe_in[]);
+void		last_cmd(t_data *data, int orig_pipe[], pid_t last_pid);
 void		first_child(t_data *data, t_cmd *cmd, int pipe_out[]);
+void		first_cmd(t_data *data, int orig_pipe[], pid_t first_pid);
 void		middle_child(t_data *data, t_cmd *cmd, int pipe_in[], int pipe_out[]);
 
 #endif
