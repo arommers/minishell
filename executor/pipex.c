1d8e4852b829d3db174416e6642a62e40750b2c1
Mirjam Goedkoop <mgoedkoo@f0r1s11.codam.nl>
2023-08-09 16:12:34 +0200

    added return values

--- executor/pipex.c
+++ executor/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mgoedkoo <mgoedkoo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/31 21:13:00 by mgoedkoo      #+#    #+#                 */
-/*   Updated: 2023/08/07 16:37:58 by mgoedkoo      ########   odam.nl         */
+/*   Updated: 2023/08/09 14:20:36 by mgoedkoo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,20 @@ static int	multi_pipes(t_data *data, int pipe_in[], pid_t *pid, int i)
 	cmd = (data->cmds)->next;
 	while (i < data->nr_pipes)
 	{
-		expand_cmd(cmd->args);
+		if (expand_cmd(cmd->args) == 1)
+			return (-1);
 		cmd->fd_io = redirects(cmd);
+		if (!cmd->fd_io)
+			return (-1);
 		if (i != 1)
 			pipe_in[0] = pipe_out[0];
 		if (pipe(pipe_out) == -1)
-			exit_error(NULL, NULL, 1);
+			return (-1);
 		if (cmd->args)
 		{
 			pid[i] = fork();
 			if (pid[i] == -1)
-				exit_error(NULL, NULL, 1);
+				return (-1);
 			if (pid[i] == 0)
 				child(data, cmd, pipe_in, pipe_out);
 		}
@@ -68,21 +71,25 @@ int	pipex(t_data *data)
 
 	pid = ft_calloc(data->nr_pipes + 1, sizeof(pid_t));
 	if (!pid)
-		exit_error(NULL, NULL, 1);
+		return (1);
 	if (pipe(orig_pipe) == -1)
-		exit_error(NULL, NULL, 1);
+		return (free(pid), 1);
 	i = 0;
 	while (i < data->nr_pipes + 1)
 	{
 		pid[i] = -1;
 		i++;
 	}
-	first_cmd(data, orig_pipe, pid[0]);
+	if (first_cmd(data, orig_pipe, pid[0]) == 1)
+		return (free(pid), 1);
 	if (data->nr_pipes > 1)
 	{
 		i = 1;
 		orig_pipe[0] = multi_pipes(data, orig_pipe, pid, i);
+		if (orig_pipe[0] == -1)
+			return (free(pid), 1);
 	}
-	last_cmd(data, orig_pipe, pid[data->nr_pipes]);
+	if (last_cmd(data, orig_pipe, pid[data->nr_pipes]) == 1)
+		return (free(pid), 1);
 	return (wait_for_children(data, pid));
 }
--- expander/expand_util.c
+++ expander/expand_util.c
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   expand_utils.c                                     :+:    :+:            */
+/*   expand_util.c                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mgoedkoo <mgoedkoo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/24 15:07:25 by mgoedkoo      #+#    #+#                 */
-/*   Updated: 2023/08/02 15:59:31 by mgoedkoo      ########   odam.nl         */
+/*   Updated: 2023/08/09 15:38:55 by mgoedkoo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	free_chrarray(char **array)
+void	*free_chrarray(char **array)
 {
 	int	i;
 
+	if (!array)
+		return (NULL);
 	i = 0;
 	while (array[i])
 	{
@@ -23,6 +25,7 @@ void	free_chrarray(char **array)
 		i++;
 	}
 	free(array);
+	return (NULL);
 }
 
 // takes temporary array and joins it in new string
@@ -38,7 +41,11 @@ char	*join_new_str(char **tmp_array)
 	{
 		tmp_str = ft_strjoin(new_str, tmp_array[i]);
 		if (!tmp_str)
-			exit_error(NULL, NULL, 1);
+		{
+			if (new_str)
+				free(new_str);
+			return (free_chrarray(tmp_array));
+		}
 		free(new_str);
 		new_str = tmp_str;
 		i++;
@@ -48,17 +55,22 @@ char	*join_new_str(char **tmp_array)
 }
 
 // loops through cmd_argv and expands each string
-void	expand_cmd(char **cmd_argv)
+int	expand_cmd(char **cmd_argv)
 {
 	int		i;
 
 	if (!cmd_argv)
-		return ;
+		return (0);
 	i = 0;
 	while (cmd_argv[i])
 	{
 		if (quote_strchr(cmd_argv[i]) || ft_strchr(cmd_argv[i], '$'))
+		{
 			cmd_argv[i] = expand_str(cmd_argv[i], 0);
+			if (!cmd_argv[i])
+				return (1);
+		}
 		i++;
 	}
+	return (0);
 }
